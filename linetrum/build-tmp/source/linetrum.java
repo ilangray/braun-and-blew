832bf5e4a883f7236afee889db7fbebf57ebe093
pbraunstein <pbraunstein12@gmail.com>
2014-11-11 11:36:03 -0500

    Got vertical segments to place vertically. For the first level of the algorithm

--- linetrum/build-tmp/source/linetrum.java
+++ linetrum/build-tmp/source/linetrum.java
@@ -238,11 +238,10 @@ class Layout {
     private final int INSET_AMOUNT = 2;
     View parent;  // Parent view
     private Point realUL;  // The actual coordinate of the UL point of canvas
-    private float canvLong;  // Length of long side of canvas
-    private float canvShort;  // Length of short side of canvas
     private float sumOfRects;  // Sum of rects to be placed
     private float scale;  // To scale up to dimenssions of the screen
-    private Boolean shortIsWidth;  // Which side is the short side?
+    private float vWidth;
+    private float vHeight;
     // TODO: Change to boolean
 
     // The floats are the unscaled area
@@ -252,10 +251,8 @@ class Layout {
     private ArrayList<View> finalViews;  // Final view for the level -- exported by class
 
 
-    public Algorithm(View parent, float canvLong, float canvShort, ArrayList<Datum> startingVals, Point realUL, Boolean shortIsWidth) {
+    public Algorithm(View parent, float vWidth, float vHeight, ArrayList<Datum> startingVals, Point realUL) {
       this.parent = parent;
-      this.canvLong = canvLong;
-      this.canvShort = canvShort;
       this.remRects = startingVals;
       this.currentRects = null;
       this.currentDatums = null;
@@ -263,180 +260,204 @@ class Layout {
       this.realUL = realUL;
       this.sumOfRects = 0;
       this.scale = 0;
-      this.shortIsWidth = shortIsWidth;
+      this.vWidth = vWidth;
+      this.vHeight = vHeight;
     }
 
-    private void squarify() {
-      // A Base case -- no remaining rects to place
+    private void squarify(boolean segmentsAreVertical) {
       if (remRects == null || remRects.isEmpty()) {
         return;
       }
 
-      // Need clean list every time
-      currentRects = new ArrayList();
-      currentDatums = new ArrayList();
-
-      sumOfRects = getSumOfRects();
-      scale = canvShort * canvLong / sumOfRects;
-
-      addFirstRect();
-      // Rect added, so need to delete it from remaining rects
-      remRects.remove(getIndexLargestRemaining());
-
-      // Loop invariant: All rectangles already in remRects have been scaled
-      // to fit the screen
-      while (!remRects.isEmpty ()) {
-        float oldWorstAR = getWorstAR(currentRects);
-        
-        // Get next rectangle
-        Datum nodeToConsider = getLargestRemaining();
-        float areaCurrent = nodeToConsider.getValueF() * scale;
-
-        // Calculate length of shared long side
-        float sharedLong = 0;
-        for (int i = 0; i < currentRects.size (); i++) {
-          sharedLong += currentRects.get(i).getArea();
-        }
-
-        sharedLong += areaCurrent;
-        sharedLong /= canvShort;
-//        println("Shared Long");
-//        println(sharedLong);
-
-        ArrayList<Rect> tempRects = new ArrayList();
-        ArrayList<Datum> tempDatums = new ArrayList();
-        float shortAreaUsedUp = 0;  // Used for placing the rectangles
-        // Put in previous rectangles
-        for (int i = 0; i < currentRects.size (); i++) {
-          Rect cRect = currentRects.get(i);
-          Datum toAdd = currentDatums.get(i);
-          shortAreaUsedUp += addNewRectToTemp(tempRects, cRect.getArea(), tempDatums, toAdd, sharedLong, shortAreaUsedUp);
-        }
-        addNewRectToTemp(tempRects, areaCurrent, tempDatums, nodeToConsider, sharedLong, shortAreaUsedUp); 
-        float newWorstAR = getWorstAR(tempRects);
-//        println("TEMPARR");
-//        testPrintRectArray(tempRects);
-//        println("END");
-        
-//        print("New Worst AR: ");
-//        println(newWorstAR);
-//        print("Old Worst AR: ");
-//        println(oldWorstAR);
-        // If next rectangle makes things worse, then GTFO
-        if (newWorstAR >= oldWorstAR) {
-          break;
-          
-        }       
-        // If next rectange would improve aspect ratio, add it in
-        currentRects = tempRects;  // Yah Garbage collection
-        currentDatums = tempDatums;
-        // Need to remove the largest one
-        remRects.remove(getIndexLargestRemaining());
-      } 
-      // Update pointUL and longSide of the canvas
-      // Update canvas dims
-      if (shortIsWidth) {
-//        println("shortIsWidth");
-        float longShared = currentRects.get(0).h;
-        realUL = realUL.offset(new Point(0, longShared));
-        canvLong -= longShared;
+      if (segmentsAreVertical) {
+        placeVerticalSegments();
       } else {
-//        println("Long is width");
-        float longShared = currentRects.get(0).w;
-        realUL = realUL.offset(new Point(longShared, 0));
-        canvLong -= longShared;
-      }
-
-      // Check if the short and long sides have swapped
-      if  (canvLong < canvShort) {  
-        shortIsWidth = !shortIsWidth;
-        float temp = canvLong;
-        canvLong = canvShort;
-        canvShort = temp;
-      } 
-//      print("New UL: (");
-//      print(realUL.x);
-//      print(", ");
-//      print(realUL.y);
-//      println(")");
-
-      // Recurse!
-      addViews();
-      squarify();
-    }
-
-    private void addViews() {
-      for (int i = 0; i < currentRects.size (); i++) {
-        Datum toAddDatum = currentDatums.get(i);
-        Rect toAddRect = currentRects.get(i).inset(INSET_AMOUNT);
-        View toAddView = new View(toAddDatum, toAddRect);
-        finalViews.add(toAddView);
+        placeHorizontalSegments();
       }
     }
+
+
+    private void placeVerticalSegments() {
+      println("In placeVerticalSegments");
+      sumOfRects = getSumOfRects();
+      scale = vWidth * vHeight / sumOfRects;
+      float rectHeight = vHeight; // Shared side
+      float xCoord = realUL.x;
+      float yCoord = realUL.x;
+
+      // Place all the rectangles
+      float widthUsed = 0;
+      while (!remRects.isEmpty()) {
+        Datum d = getLargestRemaining();
+        remRects.remove(getLargestRemaining());  // Take it out of remRects
+        float scaledArea = d.getValueF() * scale;
+        float rectWidth = scaledArea / rectHeight;
+        Rect r = new Rect(xCoord + widthUsed, yCoord, vWidth - widthUsed, vHeight);
+        finalViews.add(new View(d, r));
+
+        widthUsed += rectWidth;
+      }
+
+    }
+
+    private void placeHorizontalSegments() {
+      return;
+    }
+
+
+
+
+    
+
+    // private void _squarify() {
+    //   // A Base case -- no remaining rects to place
+    //   if (remRects == null || remRects.isEmpty()) {
+    //     return;
+    //   }
+
+    //   // Need clean list every time
+    //   currentRects = new ArrayList();
+    //   currentDatums = new ArrayList();
+
+    //   sumOfRects = getSumOfRects();
+    //   scale = canvShort * canvLong / sumOfRects;
+
+    //   addFirstRect();
+    //   // Rect added, so need to delete it from remaining rects
+    //   remRects.remove(getIndexLargestRemaining());
+
+    //   // Loop invariant: All rectangles already in remRects have been scaled
+    //   // to fit the screen
+    //   while (!remRects.isEmpty ()) {
+    //     float oldWorstAR = getWorstAR(currentRects);
+        
+    //     // Get next rectangle
+    //     Datum nodeToConsider = getLargestRemaining();
+    //     float areaCurrent = nodeToConsider.getValueF() * scale;
+
+    //     // Calculate length of shared long side
+    //     float sharedLong = 0;
+    //     for (int i = 0; i < currentRects.size (); i++) {
+    //       sharedLong += currentRects.get(i).getArea();
+    //     }
+
+    //     sharedLong += areaCurrent;
+    //     sharedLong /= canvShort;
+
+    //     ArrayList<Rect> tempRects = new ArrayList();
+    //     ArrayList<Datum> tempDatums = new ArrayList();
+    //     float shortAreaUsedUp = 0;  // Used for placing the rectangles
+    //     // Put in previous rectangles
+    //     for (int i = 0; i < currentRects.size (); i++) {
+    //       Rect cRect = currentRects.get(i);
+    //       Datum toAdd = currentDatums.get(i);
+    //       shortAreaUsedUp += addNewRectToTemp(tempRects, cRect.getArea(), tempDatums, toAdd, sharedLong, shortAreaUsedUp);
+    //     }
+    //     addNewRectToTemp(tempRects, areaCurrent, tempDatums, nodeToConsider, sharedLong, shortAreaUsedUp); 
+    //     float newWorstAR = getWorstAR(tempRects);
+
+    //     // If next rectangle makes things worse, then GTFO
+    //     if (newWorstAR >= oldWorstAR) {
+    //       break;
+          
+    //     }       
+    //     // If next rectange would improve aspect ratio, add it in
+    //     currentRects = tempRects;  // Yah Garbage collection
+    //     currentDatums = tempDatums;
+    //     // Need to remove the largest one
+    //     remRects.remove(getIndexLargestRemaining());
+    //   } 
+    //   // Update pointUL and longSide of the canvas
+    //   // Update canvas dims
+    //   if (shortIsWidth) {
+    //     float longShared = currentRects.get(0).h;
+    //     realUL = realUL.offset(new Point(0, longShared));
+    //     canvLong -= longShared;
+    //   } else {
+    //     float longShared = currentRects.get(0).w;
+    //     realUL = realUL.offset(new Point(longShared, 0));
+    //     canvLong -= longShared;
+    //   }
+
+    //   // Check if the short and long sides have swapped
+    //   if  (canvLong < canvShort) {  
+    //     shortIsWidth = !shortIsWidth;
+    //     float temp = canvLong;
+    //     canvLong = canvShort;
+    //     canvShort = temp;
+    //   } 
+
+    //   // Recurse!
+    //   addViews();
+    //   squarify();
+    // }
+
+    // private void addViews() {
+    //   for (int i = 0; i < currentRects.size (); i++) {
+    //     Datum toAddDatum = currentDatums.get(i);
+    //     Rect toAddRect = currentRects.get(i).inset(INSET_AMOUNT);
+    //     View toAddView = new View(toAddDatum, toAddRect);
+    //     finalViews.add(toAddView);
+    //   }
+    // }
 
     // Adds largest datum to currentRect and currentDatum
-    private void addFirstRect() {
-      Datum toAdd = getLargestRemaining();
-      float areaCurrent = toAdd.getValueF() * scale;
-      float longCurrent = areaCurrent / canvShort;
-      // Added extra multiplication by scale cause 2D
+    // private void addFirstRect() {
+    //   Datum toAdd = getLargestRemaining();
+    //   float areaCurrent = toAdd.getValueF() * scale;
+    //   float longCurrent = areaCurrent / canvShort;
+    //   // Added extra multiplication by scale cause 2D
 
-      // Short side isn't necessarily width or height, constructing rectangle changes based on this
-      // NOTE: rectange added in is already scaled
-      if (shortIsWidth) {
-        currentRects.add(new Rect(realUL.x, realUL.y, canvShort, longCurrent));  
-      } else {   // The height is the short side
-        currentRects.add(new Rect(realUL.x, realUL.y, longCurrent, canvShort));
-      }
-      currentDatums.add(toAdd);
-    }
+    //   // Short side isn't necessarily width or height, constructing rectangle changes based on this
+    //   // NOTE: rectange added in is already scaled
+    //   if (shortIsWidth) {
+    //     currentRects.add(new Rect(realUL.x, realUL.y, canvShort, longCurrent));  
+    //   } else {   // The height is the short side
+    //     currentRects.add(new Rect(realUL.x, realUL.y, longCurrent, canvShort));
+    //   }
+    //   currentDatums.add(toAdd);
+    // }
 
-    // Returns the worst aspect ratio in an ArayList of Rects
-    // Cann't be called on empty ArrayList  -- it will explode
-    private float getWorstAR(ArrayList<Rect> inputList) {
-      float worstAR = inputList.get(0).getAspectRatio();
+    // // Returns the worst aspect ratio in an ArayList of Rects
+    // // Cann't be called on empty ArrayList  -- it will explode
+    // private float getWorstAR(ArrayList<Rect> inputList) {
+    //   float worstAR = inputList.get(0).getAspectRatio();
 
-      for (int i = 1; i < inputList.size (); i++) {
-        float currentAR = inputList.get(i).getAspectRatio();
-        if (currentAR > worstAR) {
-          worstAR = currentAR;
-        }
-      }
+    //   for (int i = 1; i < inputList.size (); i++) {
+    //     float currentAR = inputList.get(i).getAspectRatio();
+    //     if (currentAR > worstAR) {
+    //       worstAR = currentAR;
+    //     }
+    //   }
 
-      return worstAR;
-    }
+    //   return worstAR;
+    // }
 
     // Adds in a newRect and returns the amount of the short side that was used
-    private float addNewRectToTemp(ArrayList<Rect> tempRects, float areaRect, ArrayList<Datum> tempDatums, Datum toAddDatum, float sharedLong, float shortAreaUsedUp) {
-      // Area of rectangle has already been scaled
-      float shortSideRect = areaRect / sharedLong;
+    // private float addNewRectToTemp(ArrayList<Rect> tempRects, float areaRect, ArrayList<Datum> tempDatums, Datum toAddDatum, float sharedLong, float shortAreaUsedUp) {
+    //   // Area of rectangle has already been scaled
+    //   float shortSideRect = areaRect / sharedLong;
 
-      Rect newRect;
-      if (shortIsWidth) {
-        float xCoord = realUL.x + shortAreaUsedUp;
-        float yCoord = realUL.y;
-        float rectWidth = shortSideRect;
-        float rectHeight = sharedLong;
-        newRect = new Rect(xCoord, yCoord, rectWidth, rectHeight);
-      } else {  // Short side is the height
-        float xCoord = realUL.x;
-        float yCoord = realUL.y + shortAreaUsedUp;
-        float rectWidth = sharedLong;
-        float rectHeight = shortSideRect;
-        newRect = new Rect(xCoord, yCoord, rectWidth, rectHeight);
-      }
-      tempDatums.add(toAddDatum);
+    //   Rect newRect;
+    //   if (shortIsWidth) {
+    //     float xCoord = realUL.x + shortAreaUsedUp;
+    //     float yCoord = realUL.y;
+    //     float rectWidth = shortSideRect;
+    //     float rectHeight = sharedLong;
+    //     newRect = new Rect(xCoord, yCoord, rectWidth, rectHeight);
+    //   } else {  // Short side is the height
+    //     float xCoord = realUL.x;
+    //     float yCoord = realUL.y + shortAreaUsedUp;
+    //     float rectWidth = sharedLong;
+    //     float rectHeight = shortSideRect;
+    //     newRect = new Rect(xCoord, yCoord, rectWidth, rectHeight);
+    //   }
+    //   tempDatums.add(toAddDatum);
 
-      tempRects.add(newRect);
+    //   tempRects.add(newRect);
 
-      return shortSideRect;
-    }
-    
-//    private void testPrintRectArray(ArrayList<Rect> a) {
-//      for (int i = 0; i < a.size(); i++) {
-//        println(a.get(i).toString());
-//      }
-//    }
+    //   return shortSideRect;
+    // }
 
     private Datum getLargestRemaining() {
       Datum max = remRects.get(0);
@@ -491,19 +512,19 @@ class Layout {
   }
 
   private View recurSolve(View node, int currentLevel) {
-    Boolean shortIsWidth = node.bounds.h > node.bounds.w;
-    float canvShort = shortIsWidth ? node.bounds.w : node.bounds.h;     
-    float canvLong = shortIsWidth ? node.bounds.h : node.bounds.w;
+    // Boolean shortIsWidth = node.bounds.h > node.bounds.w;
+    // float canvShort = shortIsWidth ? node.bounds.w : node.bounds.h;     
+    // float canvLong = shortIsWidth ? node.bounds.h : node.bounds.w;
     //This part used to assume that the width was always the long side of the view
-    Algorithm a = new Algorithm(node, canvLong, canvShort, copy(node.datum.children), new Point(node.bounds.x, node.bounds.y), shortIsWidth);
-    a.squarify();
+    Algorithm a = new Algorithm(node, width, height, copy(node.datum.children), new Point(node.bounds.x, node.bounds.y));
+    a.squarify(true);
     
     for (View v : a.finalViews) {
       node.subviews.add(v);
     }
     
     for (View v : node.subviews) {
-      recurSolve(v, currentLevel + 1);
+      // recurSolve(v, currentLevel + 1);
     }
     
     return node;
@@ -517,48 +538,48 @@ class Layout {
     return viewRoot;
   }
   
-  public void printTree(View node) {
-    print(node.datum.id);
-    print(": ");
-    println(node.bounds.toString());
+  // public void printTree(View node) {
+  //   print(node.datum.id);
+  //   print(": ");
+  //   println(node.bounds.toString());
     
-    for (View v : node.subviews) {
-      printTree(v); 
-    }
-  }
+  //   for (View v : node.subviews) {
+  //     printTree(v); 
+  //   }
+  // }
 
-  public void testPrintNumArray(ArrayList<Number> arr) {
-    for (int i = 0; i < arr.size (); i++) {
-      println(arr.get(i).floatValue());
-    }
-  }
+  // public void testPrintNumArray(ArrayList<Number> arr) {
+  //   for (int i = 0; i < arr.size (); i++) {
+  //     println(arr.get(i).floatValue());
+  //   }
+  // }
 
 
   // Gets all children whose parents are level 
-  private ArrayList<Datum> getChildren(int level) {
-    ArrayList<Datum> saveList = new ArrayList();
-    recurGetChildren(root, 0, level + 1, saveList);
-    return saveList;
-  }
+  // private ArrayList<Datum> getChildren(int level) {
+  //   ArrayList<Datum> saveList = new ArrayList();
+  //   recurGetChildren(root, 0, level + 1, saveList);
+  //   return saveList;
+  // }
 
   // Cannot be called on null pointer
   // 10Q: http://stackoverflow.com/questions/13349853/find-all-nodes-in-a-binary-tree-on-a-specific-level-interview-query
-  private void recurGetChildren(Datum node, int currentLev, int targetLev, ArrayList<Datum> saveList) {
-    // Target case
-    if (currentLev == targetLev) {
-      saveList.add(node);
-    }
+  // private void recurGetChildren(Datum node, int currentLev, int targetLev, ArrayList<Datum> saveList) {
+  //   // Target case
+  //   if (currentLev == targetLev) {
+  //     saveList.add(node);
+  //   }
 
-    // Base case
-    if (node.children == null) {
-      return;
-    }
+  //   // Base case
+  //   if (node.children == null) {
+  //     return;
+  //   }
 
-    // Recursion case
-    for (int i = 0; i < node.children.size(); i++) {
-      recurGetChildren(node.children.get(i), currentLev + 1, targetLev, saveList);
-    }
-  }
+  //   // Recursion case
+  //   for (int i = 0; i < node.children.size(); i++) {
+  //     recurGetChildren(node.children.get(i), currentLev + 1, targetLev, saveList);
+  //   }
+  // }
 }
 
 
