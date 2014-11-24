ab7783fa7a98554871c3561b92b6c5fb18e0b7b7
pbraunstein <pbraunstein12@gmail.com>
2014-11-24 15:14:58 -0500

    Did all the data anlysis for the first hypothesis

---
+++ a5/createAvgsPerVizPerType.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from sys import argv
+from sys import exit
+
+# Goes through the input file and averages the error 
+# for each type of graph for each user
+
+INPUT = ""
+
+def main():
+	print "Hello world"
+
+if __name__ == '__main__':
+	main()
--- a5/hyp1Analysis.r
+++ a5/hyp1Analysis.r
@@ -9,4 +9,53 @@ allData <- read.csv(file="allData.csv",head=TRUE,sep=",")
 # Trim table to be only viz type and CM error
 cols <- c(3, 6)
 allData <- allData[,cols]
-allData
+
+# Partition the matrix based on the chart type
+tm <- subset(allData, Chart_Type == 0)
+sqtm <- subset(allData, Chart_Type == 1)
+
+tmVals <- tm[,2]
+sqtmVals <- sqtm[,2]
+
+# Calculate normal distribution
+tmAvg <- mean(tmVals)
+tmStdev <- sd(tmVals)
+tmLen <- length(tmVals)
+tmError <- qnorm(0.975) * tmStdev/sqrt(tmLen)
+tmLeft <- tmAvg - tmError
+tmRight <- tmAvg + tmError
+cat("TREE MAP\n")
+cat(tmLeft, "<", tmAvg, "<", tmRight, "\n\n")
+
+sqtmAvg <- mean(sqtmVals)
+sqtmStdev <- sd(sqtmVals)
+sqtmLen <- length(sqtmVals)
+sqtmError <- qnorm(0.975) * sqtmStdev/sqrt(sqtmLen)
+sqtmLeft <- sqtmAvg - sqtmError
+sqtmRight <- sqtmAvg + sqtmError
+cat("SQUARIFIED TREE MAP")
+cat(sqtmLeft, "<", sqtmAvg, "<", sqtmRight, "\n\n")
+
+# Plot bar chart
+means <- c(tmAvg, sqtmAvg)
+lefts <- c(tmLeft, sqtmLeft)
+rights <- c(tmRight, sqtmRight)
+pdf("mapsBarPlot.pdf")
+bp <- barplot(means, names.arg = c("TM", "SQTM"), ylim = c(0, 3), main="Tree Map Error",
+	ylab="Clevland McGill Error")
+segments(bp, lefts, bp, rights, lwd=2)
+segments(bp - .1, lefts, bp + 0.1, lefts, lwd=2)
+segments(bp - .1, rights, bp + .1, rights, lwd=2)
+dev.off()
+
+# Test for normal distribution
+tmNormal <- shapiro.test(tmVals)
+sqtmNormal <- shapiro.test(sqtmVals)
+
+cat("TM VALS:\n")
+print(tmNormal[2])
+cat("SQTM VALS:\n")
+print(sqtmNormal[2])
+
+t.test(tmVals, sqtmVals)
+
