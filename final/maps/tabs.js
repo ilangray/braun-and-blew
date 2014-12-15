abd5f37e6da7b23d566942b3b550ca49c93b23d9
Ilan Gray <ilangray@outlook.com>
2014-12-15 13:12:44 -0500

    beginning of tabs

---
+++ final/maps/tabs.js
@@ -0,0 +1,39 @@
+/*
+	Manages switching between tabs and their associated filters/functionality.
+*/
+
+// listen for events, so we can change the map content based on the current tab
+$('#tabs a').click(function (e) {
+	// show the tab
+  	e.preventDefault()
+  	$(this).tab('show')
+
+  	// update the filter
+  	var name = $(this).html();
+  	onTabChange(name)
+})
+
+// maps tab names to a function that tells the map
+// to render the date appropriate for that tab
+var FILTERS = {
+	"Welcome": function () {
+		var katrinas = _.filter(DATA, function (s) {
+			return s.name === "KATRINA";
+		});
+
+		console.log("welcome page is drawing " + katrinas.length + " katrina storms");
+
+		Map.drawStorms(Map.Modes.Path, katrinas)
+	},
+
+	"Birth": function () {
+
+	},
+
+	"Explore": refreshFilter,
+}
+
+function onTabChange(tabName) {
+	// update data
+	FILTERS[tabName]()
+}
