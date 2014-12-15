8c84db092438affc812c7afa84187d9a22bad1f6
blewin1 <blewin1@gmail.com>
2014-12-15 14:59:52 -0500

    created all tabs and most have filters in place

--- final/maps/tabs.js
+++ final/maps/tabs.js
@@ -27,11 +27,49 @@ var FILTERS = {
 	},
 
 	"Birth": function () {
-		applyFilter()
+		var storms = applyFilter({
+			wind: [111, 160],
+			years: [new Date(2000,0), new Date(2008,0)]
+		})
+		Map.drawStorms(Map.Modes.Path, storms)
 	},
 
 	"Movement": function () {
-		applyFilter()
+		var storms = applyFilter({
+			wind: [118, 158],
+			years: [new Date(1993,0), new Date(2000,0)]
+		})
+		Map.drawStorms(Map.Modes.Path, storms)
+	},
+
+	"Death": function () {
+
+		var toInclude = [
+						{name: "KARL", year: 2004}
+						{name: "OPHELIA", year: 2005}
+						{name: "ALBERTO", year: 2000}
+						{name: "CHARLEY", year: 1986}
+						{name: "KATIA", year: 2011}
+						{name: "LILI", year: 1996}
+						{name: "ISABEL", year: 2003}
+						{name: "GLIBERT", year: 1988}
+						{name: "IKE", year: 2008}
+						{name: "ISAAC", year: 2012}
+						{name: "LILI", year: 2002}
+						{name: "FRANCES", year: 2004}
+						];
+
+
+
+		Map.drawStorms(Map.Modes.Heat, storms)
+	},
+
+	"Strength": function () {
+		var storms = applyFilter({
+			wind: [125, 160],
+			years: [new Date(1985,0), new Date(2000,0)]
+		})
+		Map.drawStorms(Map.Modes.Heat, storms)
 	},
 
 	"Explore": refreshFilter,
