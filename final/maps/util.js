b4cd01ca9653fcdc333c8585543889e23b5d4b8f
Ilan Gray <ilangray@outlook.com>
2014-12-15 14:57:09 -0500

    filterStorms util function

--- final/maps/util.js
+++ final/maps/util.js
@@ -3,3 +3,24 @@ function getYear(storm) {
     var date = new Date(storm.data[0].date);
     return date.getFullYear();
 }
+
+function getName(storm) {
+	return storm.name;
+}
+
+// storms is an array of objects containing
+// 'year' and 'name' properties
+function filterStorms(storms) {
+	// returns true iff the query is in the given list of storms
+	var matches = function (query) {
+		var year = getYear(query);
+		var name = getName(query);
+
+		return _.any(storms, function (s) {
+			return s.year === year && s.name === name;
+		});
+	}
+
+
+	return _.filter(DATA, matches);
+}
